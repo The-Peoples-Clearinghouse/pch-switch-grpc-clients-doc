924680fa870b5dffdc4cf6ddbe1e4165b17a4782
Jorge Sebastian Miguel <19161356@itoaxaca.edu.mx>
2026-09-25 20:37:54 -0600

    Refactor offline payments documentation to replace "Cliente" with "Usuario" for consistency. - Updated descriptions and parameters in the offline funds reservation, release, and settle handlers. - Removed outdated request attestation challenge documentation. - Added new sections for app specifications, token creation, device onboarding, and processing offline transfer certificates. - Enhanced clarity on the request and response structures across various programming languages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -53,8 +53,30 @@ export default defineConfig({
 						},
 						{
 							label: 'Pagos offline',
-							autogenerate: { directory: 'guides/offline-payments' },
 							collapsed: false,
+							items: [
+								{
+									label: 'Introducción',
+									link: '/guides/offline-payments/introduction',
+								},
+								{
+									label: 'Reservar fondos para uso offline',
+									link: '/guides/offline-payments/offline-funds-reservation-handler',
+								},
+								{
+									label: 'Acreditar un pago offline',
+									link: '/guides/offline-payments/settle-offline-transfers-handler',
+								},
+								{
+									label: 'Liberar reserva offline',
+									link: '/guides/offline-payments/release-offline-funds-reservation-handler',
+								},
+								{
+									label: 'Integración con app propia',
+									autogenerate: { directory: 'guides/offline-payments/own-app' },
+									collapsed: true,
+								}
+							],
 						},
 					],
 				},
